9cd06372f0421a228f1e1c0f13f4d7cf0c796168
Jericoth Castellar <143036192+JericothCastellar@users.noreply.github.com>
2023-11-10 16:44:23 -0500

    Add files via upload

---
+++ Ejercicio 1/src/ejercicio/pkg1/Ejercicio1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Muestra el mensaje:
+           JOptionPane.showMessageDialog(null, "Fundamentos de programacion");
+     
+    }
+    
+}
