8aa2a02de9d8c260083584d70354506736c8587d
Jericoth Castellar <143036192+JericothCastellar@users.noreply.github.com>
2023-11-10 17:02:47 -0500

    Add files via upload

---
+++ Ejercicios con interfaz grafica/Ejercicio120130.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.pkg120.pkg130;
+
+/**
+ *
+ * @author JERICOTH
+ */
+public class Ejercicio120130 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Dos y dos son cuadro, cuadro y dos son seis, seis y dos son ocho y ocho dieciseis
+    }
+    
+}
